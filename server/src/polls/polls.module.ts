544f8ce95287f6438e0c20a0d35038790616e6b4
slava.odinokov <odin.slavy@incodewetrust.ru>
2023-07-24 23:39:40 +0300

    feat: connected to redis

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { RedisModule } from 'src/redis/redis.module';
+import { getRedisConfig } from 'src/configs/redis.config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    RedisModule.registerAsync(getRedisConfig()),
+  ],
   controllers: [PollsController],
   providers: [PollsService],
 })
