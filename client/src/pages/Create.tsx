298af3607c0109bc614beb9dbb8bd29c126e8ec7
slava.odinokov <odin.slavy@incodewetrust.ru>
2023-07-28 00:08:33 +0300

    feat/client: added Welcom, Create and Join pages

---
+++ client/src/pages/Create.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Create: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-around items-stretch w-full h-full mx-auto max-w-sm">
+      <h3 className="text-center">Enter Poll Topic</h3>
+    </div>
+  );
+};
+
+export default Create;
---
+++ client/src/pages/Join.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Join: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-around items-stretch w-full h-full mx-auto max-w-sm">
+      <h3 className="text-center">Enter Code Provided by &quot;Freind&quot;</h3>
+    </div>
+  );
+};
+
+export default Join;
